b67d6d7959c94ca1a83f8d5defd1b346a5f20740
ATirado <azuru5039@gmail.com>
2023-11-28 13:52:25 -0500

    add comment template and create function

--- z_app/urls.py
+++ z_app/urls.py
@@ -5,7 +5,9 @@ from . import views
 urlpatterns = [
    path('',views.home, name='home'),
    path('tweets/<int:tweet_id>/comments/', views.comment_list, name='comment_list'),
-   path('tweets/<int:tweet_id>/comments/<int:pk>/', views.comment_detail, name='comment_detail'),
+   path('tweets/<int:tweet_id>/comments/<int:comment_id>/', views.comment_detail, name='comment_detail'),
    path('tweets/<int:tweet_id>/comments/new/', views.comment_create, name='comment_create'),
+   path('tweets/<int:tweet_id>/comments/<int:comment_id>/edit/', views.comment_update, name='comment_update'),
+   path('tweets/<int:tweet_id>/comments/<int:comment_id>/delete/', views.comment_delete, name='comment_delete'),
 ]
 
--- z_app/views.py
+++ z_app/views.py
@@ -12,4 +12,16 @@ def comment_list(request, tweet_id):
     comments = tweet.comments.all()
     return render(request, 'z_app/comment_list.html', {'comments': comments, 'tweet': tweet})
 
-    
+def comment_create(request, tweet_id):
+    tweet = get_object_or_404(Tweet, pk=tweet_id)
+    if request.method == "POST":
+        form = CommentForm(request.POST)
+        if form.is_valid():
+            comment = form.save(commit=False)
+            comment.tweet = tweet
+            comment.user = request.user
+            comment.save()
+            return redirect('comment_detail', tweet_id=tweet_id, comment_id=comment.pk)
+    else:
+        form = CommentForm()
+    return render(request, 'z_app/comment_form.html', {'form': form, 'tweet': tweet})
