4baa7752ed68ec63f571372e36eb5973db789e58
Josehneves <josehneves28@gmail.com>
2023-11-29 11:58:53 -0500

    tweet linked to the home page

--- z_app/urls.py
+++ z_app/urls.py
@@ -7,7 +7,11 @@ from django.urls import path, include
 urlpatterns = [
    path('',views.home, name='home'),
    path('admin/', admin.site.urls),
-   path('tweets/<int:tweet_id>/comments/', views.comment_list, name='comment_list'),
+   path('tweets/', views.tweet_list, name='tweet_list'),
+   path('tweet/<int:pk>/', views.tweet_detail, name='tweet_detail'),
+   path('tweet/new/', views.tweet_new, name='tweet_new'),
+   path('tweet/<int:pk>/edit/', views.tweet_edit, name='tweet_edit'),
+   path('tweet/<int:pk>/delete/', views.tweet_delete, name='tweet_delete'),
    path('tweets/<int:tweet_id>/comments/<int:comment_id>/', views.comment_detail, name='comment_detail'),
    path('tweets/<int:tweet_id>/comments/new/', views.comment_create, name='comment_create'),
    path('tweets/<int:tweet_id>/comments/<int:comment_id>/edit/', views.comment_update, name='comment_update'),
