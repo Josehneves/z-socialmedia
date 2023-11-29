0d9aba5201b3fb87e74ea6dd00137165bb70c385
Josehneves <josehneves28@gmail.com>
2023-11-28 21:02:21 -0500

    tweet model and crud

--- z_app/urls.py
+++ z_app/urls.py
@@ -1,7 +1,11 @@
 # z_app
 from django.urls import path
 from . import views
+from django.contrib import admin
+from django.urls import path, include
 
 urlpatterns = [
    path('',views.home, name='home'),
+   path('admin/', admin.site.urls),
+   path('zsocialmedia/', include('zsocialmedia.urls')),
 ]
---
+++ zsocialmedia/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class ZsocialmediaConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'zsocialmedia'
