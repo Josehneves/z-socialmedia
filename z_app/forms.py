7951440180cbd7fe9e0cfee076e474441570ce6b
ATirado <azuru5039@gmail.com>
2023-11-29 17:27:28 -0500

    add comment form

--- z_app/forms.py
+++ z_app/forms.py
@@ -1,7 +1,16 @@
 from django import forms
 from .models import Tweet
+from .models import Comment
 
 class TweetForm(forms.ModelForm):
     class Meta:
         model = Tweet
         fields = ('content',)
+
+class CommentForm(forms.ModelForm):
+    class Meta:
+        model = Comment
+        fields = ['text']
+        widgets = {
+            'text': forms.Textarea(attrs={'placeholder': 'Write your comment here'}),
+        }
