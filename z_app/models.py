43a698c53daf0c3b0840e7add098279b3ff0eb24
Ezekeil Pahila <ezekeilvpahila@gmail.com>
2023-11-30 22:10:26 -0500

    Add registration and login

--- z_app/models.py
+++ z_app/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.db.models.signals import post_save
 
 # Create User Profile Model 
 class UserProfile(models.Model):
@@ -7,6 +8,17 @@ class UserProfile(models.Model):
     follows = models.ManyToManyField("self",
         related_name="followed_by",symmetrical=False,
         blank=True)
+    data_modified = models.DateTimeField(user, auto_now=True)
+    join_date = models.DateTimeField(user, auto_now_add=True)
     
     def __str__(self):
         return self.user.username
+# Auto create profile and follow user itself when created.
+def create_profile(sender, instance, created, **kwargs):
+    if created:
+        user_profile = UserProfile(user=instance)
+        user_profile.save()
+        user_profile.follows.set([instance.userprofile.id])
+        user_profile.save()
+
+post_save.connect(create_profile, sender=User)
--- z_app/views.py
+++ z_app/views.py
@@ -1,6 +1,70 @@
 # z_app views
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib import messages
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.forms import UserCreationForm
+from django import forms
+from .forms import SignUpForm
+from .models import UserProfile
+
+
 
 # Create your views here.
 def home(request):
     return render(request, 'home.html', {})
+
+
+def profiles(request):
+    if request.user.is_authenticated:
+        profile_list = UserProfile.objects.exclude(user=request.user)
+        return render(request, 'profiles.html', {"Profiles":profile_list})
+    else:
+        messages.success(request,"Please create an account to view this page or login if you have an existing account with us.")
+        return redirect('home')
+
+def profile(request,pk):
+    if request.user.is_authenticated:
+        profile = UserProfile.objects.get(user_id=pk)
+        return render(request, 'profile.html',{"profile":profile})
+    else:
+        messages.success(request,"Please create an account to view this page or login if you have an existing account with us.")
+        return redirect('home')
+    
+
+def login_user(request):
+    if request.method == "POST":
+        username = request.POST['username']
+        password = request.POST['password']
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            messages.success(request,"Successful login.")
+            return redirect('home')
+        else:
+            messages.success(request,"Login failed.")
+            return redirect('login')
+    else:
+        return render(request, 'login.html',{})
+
+def logout_user(request):
+    logout(request)
+    messages.success(request,"User logged out.")
+    return redirect('login')
+
+def register_newuser(request):
+    form = SignUpForm()
+    if request.method == "POST":
+        form = SignUpForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data['username']
+            password = form.cleaned_data['password1']
+            first_name = form.cleaned_data['first_name']
+            last_name = form.cleaned_data['last_name']
+            email = form.cleaned_data['email']
+            user = authenticate(username=username, password=password)
+            login(request, user)
+            messages.success(request,"Successfully created an account.")
+            return redirect('home')
+        
+    return render(request, 'register.html', {'form':form})
