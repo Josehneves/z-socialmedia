053bbe1a7ee2fd12255b9f06f281309edddd65c8
Ezekeil Pahila <ezekeilvpahila@gmail.com>
2023-12-01 09:15:01 -0500

    Merge styling

--- z_app/forms.py
+++ z_app/forms.py
@@ -46,23 +46,5 @@ class CommentForm(forms.ModelForm):
         fields = ['text']
         widgets = {
             'text': forms.Textarea(attrs={'placeholder': 'Write your comment here'}),
-        }from django import forms
-from .models import Tweet
-from .models import Comment
 
-class TweetForm(forms.ModelForm):
-    class Meta:
-        model = Tweet
-        fields = ('user', 'content',)
-        widgets = {
-            'user': forms.HiddenInput(),
-            'content': forms.Textarea(attrs={'placeholder': 'Write your tweet here'}),
-        }
 
-class CommentForm(forms.ModelForm):
-    class Meta:
-        model = Comment
-        fields = ['text']
-        widgets = {
-            'text': forms.Textarea(attrs={'placeholder': 'Write your comment here'}),
-        }
